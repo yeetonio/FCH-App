d00f113d7ca3ab218ceee4087b2af9885ca5de93
yeetonio <yeetonio@gmail.com>
2025-10-27 19:30:21 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('fch-v1').then(cache => cache.addAll(['/', '/index.html', '/manifest.json', '/icon.png'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
